a292380889452ef2646424c993f3a5dae52d588a
worldken <ken.inoue07@gmail.com>
2020-09-18 02:58:59 -0500

    -back button functionality at AddQuestionFragment.kt

--- app/src/main/java/com/kenruizinoue/flashcardapp/view/activity/MainActivity.kt
+++ app/src/main/java/com/kenruizinoue/flashcardapp/view/activity/MainActivity.kt
@@ -2,7 +2,7 @@ package com.kenruizinoue.flashcardapp.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,6 +11,9 @@ import com.kenruizinoue.flashcardapp.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,9 +21,14 @@ class MainActivity : AppCompatActivity() {
         setupBottomNavBar()
     }
 
+    // enable back button functionality
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
     private fun setupBottomNavBar() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         // set all the fragments as the top-level destinations
         val appBarConfiguration = AppBarConfiguration(bottomNavView.menu)
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/kenruizinoue/flashcardapp/view/fragment/AddQuestionFragment.kt
+++ app/src/main/java/com/kenruizinoue/flashcardapp/view/fragment/AddQuestionFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.kenruizinoue.flashcardapp.R
@@ -25,7 +26,6 @@ class AddQuestionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val saveButton = view.findViewById<Button>(R.id.saveQuestionButton)
         val questionEditText = view.findViewById<EditText>(R.id.questionEditText)
         val answerEditText = view.findViewById<EditText>(R.id.answerEditText)
