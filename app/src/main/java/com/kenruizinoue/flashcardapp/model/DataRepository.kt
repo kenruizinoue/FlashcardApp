d172a0b0968b87ce3b0690b1053c7fd989899f39
worldken <ken.inoue07@gmail.com>
2020-09-26 16:35:09 -0500

    inject ViewModel pt2: -updated DataRepository.kt -used DataRepository at QuestionDetailViewModel.kt

--- app/src/main/java/com/kenruizinoue/flashcardapp/model/DataRepository.kt
+++ app/src/main/java/com/kenruizinoue/flashcardapp/model/DataRepository.kt
@@ -13,6 +13,18 @@ class DataRepository @Inject constructor(private val questionCardDao: QuestionCa
         return questionCardDao.getCards()
     }
 
+    fun getCardById(id: Int): LiveData<QuestionCard> {
+        return questionCardDao.getCardById(id)
+    }
+
+    suspend fun updateQuestionCard(questionCard: QuestionCard) {
+        questionCardDao.updateQuestionCard(questionCard)
+    }
+
+    suspend fun deleteById(id: Int) {
+        questionCardDao.deleteById(id)
+    }
+
     suspend fun getCardList(): List<QuestionCard> {
         return questionCardDao.getCardList()
     }
--- app/src/main/java/com/kenruizinoue/flashcardapp/viewModel/QuestionDetailViewModel.kt
+++ app/src/main/java/com/kenruizinoue/flashcardapp/viewModel/QuestionDetailViewModel.kt
@@ -3,24 +3,24 @@ package com.kenruizinoue.flashcardapp.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.kenruizinoue.flashcardapp.model.DataRepository
 import com.kenruizinoue.flashcardapp.model.QuestionCard
-import com.kenruizinoue.flashcardapp.model.QuestionCardDao
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class QuestionDetailViewModel @Inject constructor(private val questionCardDao: QuestionCardDao) : ViewModel() {
+class QuestionDetailViewModel @Inject constructor(private val dataRepository: DataRepository) : ViewModel() {
 
-    fun getQuestionById(id: Int): LiveData<QuestionCard> = questionCardDao.getCardById(id)
+    fun getQuestionById(id: Int): LiveData<QuestionCard> = dataRepository.getCardById(id)
 
     fun startUpdate(id: Int, question: String, answer: String): Boolean {
         if (!(question.isNotBlank() && answer.isNotBlank())) return false
         viewModelScope.launch {
-            questionCardDao.updateQuestionCard(QuestionCard(id, question, answer))
+            dataRepository.updateQuestionCard(QuestionCard(id, question, answer))
         }
         return true
     }
 
     fun startDelete(id: Int) {
-        viewModelScope.launch { questionCardDao.deleteById(id) }
+        viewModelScope.launch { dataRepository.deleteById(id) }
     }
 }
